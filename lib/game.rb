5173f31baa59c5eca7388f35a02c9d8a887f3ab9
clay-town <justinmccune@icloud.com>
2020-06-09 09:50:41 -0700

    first commit, forgot to do this throughout the test

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,32 @@
 require_relative 'board'
 
-class Fixme
+class Game
+	attr_accessor :board, :over
+
+	@over = false
+	
+
+	def initialize(board = Board.new)
+		@board = board
+    if (board == 'FAKE' || board.full?)
+      @over = true
+		end
+	end
+
+  def tie?
+    # @board.token_at(:top, :left))
+    return true
+  end
+
+  def winner
+    if @board.token_at(:top, :left) == :x && @board.token_at(:top, :middle) == x && @board.token_at(:top, :right) == :x 
+      return :x
+    end
+  end
+
+	def over?
+		return @over
+	end
 end
+
+
